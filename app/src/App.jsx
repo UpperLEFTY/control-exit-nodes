a2386c53d3d5cb38a2149c1e11bd7c2137afd1ea
Essence Alexander <upperlefty@zoho.eu>
2023-01-25 22:18:06 -0800

    files not needed cleanup

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,22 +1,13 @@
 /* eslint-disable no-shadow */
 /* eslint-disable react/button-has-type */
 import React, { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <div />
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
